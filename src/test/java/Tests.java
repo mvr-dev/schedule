8b30506e91eb468392d8bf259afe626abcea986e
Vyacheslav Markov <slavacor8001@gmail.com>
2025-09-14 21:27:48 +0600

    delete tests

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -8,17 +8,9 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class Tests {
-    @Test
-    public void testRegex(){
 
-        assertTrue(Utils.testGroup("ХХБ-001-О-01"));
-    }
 
-    @Test
-    public void testRegex2(){
-        Bot bot = new Bot();
-        assertFalse(Utils.testGroup("ЁЖЗ-999-Щ-99"));
-    }
+
     @Test
     public void testRequest(){
         System.out.println(RequestUtil.getOmstuGroupJson("ПРД-231"));
