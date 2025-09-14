743872a12f8975d1a2daa5ddafcf8fe9152badd8
Vyacheslav Markov <slavacor8001@gmail.com>
2025-09-14 11:11:44 +0600

    some updates

--- src/main/java/dev/mvr/schedule/Bot.java
+++ src/main/java/dev/mvr/schedule/Bot.java
@@ -8,16 +8,20 @@ import com.vk.api.sdk.exceptions.ClientException;
 import com.vk.api.sdk.httpclient.HttpTransportClient;
 import com.vk.api.sdk.objects.messages.Message;
 import com.vk.api.sdk.queries.messages.MessagesGetLongPollHistoryQuery;
+import jakarta.servlet.annotation.WebListener;
 
 import java.util.List;
 import java.util.Random;
 
+@WebListener
 public class Bot {
-    public static void main(String[] args) throws ClientException, ApiException, InterruptedException {
+    public void runBot() throws ClientException, ApiException, InterruptedException {
         TransportClient transportClient = new HttpTransportClient();
         VkApiClient vk = new VkApiClient(transportClient);
-        String token = System.getenv("VK_BOT_TOKEN");
-        Integer groupId = Integer.parseInt(System.getenv("VK_GROUP_ID"));
+        String token =
+                System.getenv("VK_BOT_TOKEN");
+        Integer groupId =
+                Integer.parseInt(System.getenv("VK_GROUP_ID"));
         GroupActor actor = new GroupActor(groupId,token);
         Integer ts = vk.messages().getLongPollServer(actor).execute().getTs();
         Random random = new Random();
@@ -29,6 +33,7 @@ public class Bot {
                 messages.forEach(message -> {
                             try {
                                 if(message.getText().equals("Привет")){
+//                                    System.out.println("Get Привет");
                                     vk.messages()
                                             .send(actor)
                                             .message("Привет от бота")
@@ -37,6 +42,7 @@ public class Bot {
                                             .execute();
                                 }
                                 else {
+//                                    System.out.println("Get "+message.getText());
                                     vk.messages()
                                             .send(actor)
                                             .message(message.getText())
